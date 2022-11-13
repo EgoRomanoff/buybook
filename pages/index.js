40bc4ce0e41664d5e4afcbd10a60d6a8b7b246a1
EgoRomanoff <egoromanoff@mail.ru>
2022-11-13 15:35:52 +0300

    feat: add css-modules and content

--- pages/index.js
+++ pages/index.js
@@ -1,43 +1,65 @@
 import Image from 'next/image'
 import { Container } from 'react-bootstrap'
+import cls from '../styles/Main.module.scss'
 
 export default function Home() {
   return (
     <>
-      <header className="header
-                         bg-light">
-        <Container>
-          <div className="header__inner
+      <header className={ [cls.header,
+                          'bg-light',
+                          'fixed-top']
+                            .join(' ')}>
+        <Container className='h-100'>
+          <div className='header__inner
                           d-flex
                           flex-row
                           align-items-center
-                          py-4">
+                          h-100'>
             HEADER
           </div>
         </Container>
       </header>
 
-      <main className="main
-                       d-flex
-                       flex-column
-                       h-100
-                       align-items-center
-                       py-4">
+      <main className={ [cls.main,
+                        'd-flex',
+                        'flex-column',
+                        'h-100',
+                        'align-items-center',
+                        'py-4']
+                          .join(' ') }>
 
         <Container>
 
           <div className='main__inner'>
 
-          <section class="greeting
-                          w-100">
-            <h1 className='fs-1
-                           fw-normal'>
-              <span className='text-primary
-                               fw-semibold'>
-                BuyBook
-              </span> welcomes You!
-            </h1>
-          </section>
+            <section className={ cls.greeting + ' d-flex align-items-center position-relative'}>
+              <h1 className='fs-1
+                            fw-normal
+                            text-center
+                            flex-fill'>
+                Welcome to<br></br>
+                <span className='text-primary
+                                fw-semibold'>
+                  BuyBook
+                </span>
+              </h1>
+              <Image
+                  className='position-absolute
+                            top-25
+                            start-0'
+                  src='/book-1.jpg'
+                  width='128'
+                  height='74'
+                  alt='Book' />
+              <Image
+                  className='position-absolute
+                            top-50
+                            end-0'
+                  src='/book-2.jpg'
+                  width='160'
+                  height='100'
+                  alt='Book' />
+            </section>
 
           </div>
 
